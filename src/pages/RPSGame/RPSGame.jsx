d39323de8d8916abfc3927442a612c559aaa7cb7
Dmitrii <dimterion59@gmail.com>
2024-01-21 15:23:28 +0100

    adjust rpsGame win condition, add hearts icons, change texts

--- src/pages/RPSGame/RPSGame.jsx
+++ src/pages/RPSGame/RPSGame.jsx
@@ -1,5 +1,13 @@
 import { useState } from "react";
-import { FaHandPaper, FaHandRock, FaHandScissors } from "react-icons/fa";
+import {
+  FaHandPaper,
+  FaHandRock,
+  FaHandScissors,
+  FaHeart,
+  FaRegHeart,
+} from "react-icons/fa";
+import { TbArrowBigDownLinesFilled } from "react-icons/tb";
+import { VscDebugRestart } from "react-icons/vsc";
 import { roll } from "./utils";
 import player from "../../assets/RPSGame/player.jpg";
 import opponent from "../../assets/RPSGame/opponent.jpg";
@@ -11,10 +19,66 @@ import Header from "../../components/Header/Header";
 import "./rpsGame.css";
 
 export default function RPSGame() {
+  const [playerScore, setPlayerScore] = useState(3);
+  const [opponentScore, setOpponentScore] = useState(3);
   const [gameStatus, setGameStatus] = useState("Choose to start");
   const [playerRoll, setPlayerRoll] = useState("question mark");
   const [opponentRoll, setOpponentRoll] = useState("question mark");
   const [rollAnimation, setRollAnimation] = useState(false);
+
+  const playerHearts =
+    playerScore === 3 ? (
+      <div className="rpsGame-heartsContainer">
+        <FaHeart />
+        <FaHeart />
+        <FaHeart />
+      </div>
+    ) : playerScore === 2 ? (
+      <div className="rpsGame-heartsContainer">
+        <FaHeart />
+        <FaHeart />
+        <FaRegHeart />
+      </div>
+    ) : playerScore === 1 ? (
+      <div className="rpsGame-heartsContainer">
+        <FaHeart />
+        <FaRegHeart />
+        <FaRegHeart />
+      </div>
+    ) : (
+      <div className="rpsGame-heartsContainer">
+        <FaRegHeart />
+        <FaRegHeart />
+        <FaRegHeart />
+      </div>
+    );
+
+  const opponentHearts =
+    opponentScore === 3 ? (
+      <div className="rpsGame-heartsContainer">
+        <FaHeart />
+        <FaHeart />
+        <FaHeart />
+      </div>
+    ) : opponentScore === 2 ? (
+      <div className="rpsGame-heartsContainer">
+        <FaHeart />
+        <FaHeart />
+        <FaRegHeart />
+      </div>
+    ) : opponentScore === 1 ? (
+      <div className="rpsGame-heartsContainer">
+        <FaHeart />
+        <FaRegHeart />
+        <FaRegHeart />
+      </div>
+    ) : (
+      <div className="rpsGame-heartsContainer">
+        <FaRegHeart />
+        <FaRegHeart />
+        <FaRegHeart />
+      </div>
+    );
 
   const playerRolls = ["rock", "paper", "scissors"];
 
@@ -41,9 +105,17 @@ export default function RPSGame() {
       <button
         key={playerRoll}
         onClick={() => {
-          roll(playerRoll, setPlayerRoll, setOpponentRoll, setGameStatus);
+          roll(
+            playerRoll,
+            setPlayerRoll,
+            setOpponentRoll,
+            setPlayerScore,
+            setOpponentScore,
+            setGameStatus
+          );
           setRollAnimation(true);
         }}
+        disabled={playerScore === 0 || opponentScore === 0}
         className="rpsGame-btn"
       >
         {playerRoll === "rock" ? (
@@ -62,44 +134,87 @@ export default function RPSGame() {
       <Header />
       <section className="rpsGame-section">
         <div className="rpsGame-playersInfoContainer">
-          <div>
+          <div className="rpsGame-playersInfoContainerItem">
+            <h3>Player</h3>
             <img
               src={player}
               alt="Futuristic android with a pen and tablet"
               className="rpsGame-portrait"
             />
-            <h3>Player</h3>
+            {playerHearts}
           </div>
-          <div>
+          <div className="rpsGame-playersInfoContainerItem">
+            <h3>Opponent</h3>
             <img
               src={opponent}
               alt="Futuristic android with a pen and tablet"
               className="rpsGame-portrait"
             />
-            <h3>Opponent</h3>
+            {opponentHearts}
           </div>
         </div>
         <h1 className="rpsGame-heading">Rock, Paper, Scissors</h1>
-        <div>
-          <img
-            src={playerRollImg}
-            alt={`Futuristic ${playerRoll}`}
-            className={
-              rollAnimation === true ? "rpsGame-imgAnimation" : "rpsGame-img"
-            }
-            onAnimationEnd={() => setRollAnimation(false)}
-          />
-          <img
-            src={opponentRollImg}
-            alt={`Futuristic ${opponentRoll}`}
-            className={
-              rollAnimation === true ? "rpsGame-imgAnimation" : "rpsGame-img"
-            }
-            onAnimationEnd={() => setRollAnimation(false)}
-          />
-        </div>
-        <h2 className="rpsGame-subHeading">{gameStatus}</h2>
+        {playerScore === 0 ? (
+          <div>
+            <img
+              src={opponent}
+              alt="Futuristic android with a pen and tablet"
+              className="rpsGame-img"
+            />
+          </div>
+        ) : opponentScore === 0 ? (
+          <div>
+            <img
+              src={player}
+              alt="Futuristic android with a pen and tablet"
+              className="rpsGame-img"
+            />
+          </div>
+        ) : (
+          <div>
+            <img
+              src={playerRollImg}
+              alt={`Futuristic ${playerRoll}`}
+              className={
+                rollAnimation === true ? "rpsGame-imgAnimation" : "rpsGame-img"
+              }
+              onAnimationEnd={() => setRollAnimation(false)}
+            />
+            <img
+              src={opponentRollImg}
+              alt={`Futuristic ${opponentRoll}`}
+              className={
+                rollAnimation === true ? "rpsGame-imgAnimation" : "rpsGame-img"
+              }
+              onAnimationEnd={() => setRollAnimation(false)}
+            />
+          </div>
+        )}
+        <h2 className="rpsGame-subHeading">
+          {gameStatus === "Choose to start" && <TbArrowBigDownLinesFilled />}
+          {playerScore === 0
+            ? "Opponent is victorious!"
+            : opponentScore === 0
+            ? "You are victorious!"
+            : gameStatus}
+          {gameStatus === "Choose to start" && <TbArrowBigDownLinesFilled />}
+        </h2>
         <div className="rpsGame-buttons">{buttons}</div>
+        {(playerScore === 0 || opponentScore === 0) && (
+          <button
+            onClick={() => {
+              setPlayerScore(3);
+              setOpponentScore(3);
+              setGameStatus("Choose to start");
+              setPlayerRoll("question mark");
+              setOpponentRoll("question mark");
+              setRollAnimation(true);
+            }}
+            className="rpsGame-btn"
+          >
+            <VscDebugRestart />
+          </button>
+        )}
       </section>
     </>
   );
--- src/pages/RPSGame/utils.js
+++ src/pages/RPSGame/utils.js
@@ -2,6 +2,8 @@ export function roll(
   playerRoll,
   setPlayerRoll,
   setOpponentRoll,
+  setPlayerScore,
+  setOpponentScore,
   setGameStatus
 ) {
   setPlayerRoll(playerRoll);
@@ -15,17 +17,23 @@ export function roll(
   let gameStatus;
 
   if (playerRoll === "rock" && opponentRoll === "scissors") {
-    gameStatus = "You win!";
+    gameStatus = "You win the round!";
+    setOpponentScore((prevOpponentScore) => prevOpponentScore - 1);
   } else if (playerRoll === "rock" && opponentRoll === "paper") {
-    gameStatus = "Opponent wins!";
+    gameStatus = "Opponent wins the round!";
+    setPlayerScore((prevPlayerScore) => prevPlayerScore - 1);
   } else if (playerRoll === "paper" && opponentRoll === "rock") {
-    gameStatus = "You win!";
+    gameStatus = "You win the round!";
+    setOpponentScore((prevOpponentScore) => prevOpponentScore - 1);
   } else if (playerRoll === "paper" && opponentRoll === "scissors") {
-    gameStatus = "Opponent wins!";
+    gameStatus = "Opponent wins the round!";
+    setPlayerScore((prevPlayerScore) => prevPlayerScore - 1);
   } else if (playerRoll === "scissors" && opponentRoll === "paper") {
-    gameStatus = "You win!";
+    gameStatus = "You win the round!";
+    setOpponentScore((prevOpponentScore) => prevOpponentScore - 1);
   } else if (playerRoll === "scissors" && opponentRoll === "rock") {
-    gameStatus = "Opponent wins!";
+    gameStatus = "Opponent wins the round!";
+    setPlayerScore((prevPlayerScore) => prevPlayerScore - 1);
   } else {
     gameStatus = "Tie!";
   }
