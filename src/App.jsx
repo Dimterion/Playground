2ebb3364caf5b1074c87ce15b04564e3c98c3862
Dmitrii <dimterion59@gmail.com>
2024-01-26 10:12:59 +0100

    add initial components/routes for a new page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import RPSGame from "./pages/RPSGame/RPSGame";
+import CardsGame from "./pages/CardsGame/CardsGame";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/rps-game" element={<RPSGame />} />
+        <Route path="/cards-game" element={<CardsGame />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/CardsGame/CardsGame.jsx
@@ -0,0 +1,15 @@
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+import "./cardsGame.css";
+
+export default function CardsGame() {
+  return (
+    <div className="cardsGame-container">
+      <Header />
+      <section>
+        <h1>Cards Game</h1>
+      </section>
+      <Footer />
+    </div>
+  );
+}
