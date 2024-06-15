77d139a1860371b96f6bdf94681d7b54cf70bf8c
Dimterion <dimterion59@gmail.com>
2024-06-15 09:09:39 +0300

    add propTypes, adjust footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,24 @@
+import PropTypes from "prop-types";
 import { FaRegCopyright } from "react-icons/fa";
 import "./footer.css";
 
-export default function Footer() {
+export default function Footer({ link }) {
   return (
-    <a
-      href="https://github.com/Dimterion"
-      target="_blank"
-      rel="noopener noreferrer"
-      className="footer-link"
-    >
-      <FaRegCopyright />
-      Dimterion
-    </a>
+    <footer>
+      <a>{link}</a>
+      <a
+        href="https://github.com/Dimterion"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="footer-link"
+      >
+        <FaRegCopyright />
+        Dimterion
+      </a>
+    </footer>
   );
 }
+
+Footer.propTypes = {
+  link: PropTypes.string,
+};
