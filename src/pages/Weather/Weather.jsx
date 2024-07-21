c6c25cfc8f55d668dfcb123ab65c0922a4d8e287
Dmitrii <dimterion59@gmail.com>
2024-07-21 19:54:45 +0200

    add initial code for fetch request

--- src/pages/Weather/Weather.jsx
+++ src/pages/Weather/Weather.jsx
@@ -1,7 +1,41 @@
+import { useState } from "react";
 import "./weather.css";
 
 function Weather() {
-  return <h2>Weather</h2>;
+  // const apiKey = import.meta.env.WEATHER_API_KEY;
+  const [city, setCity] = useState("");
+
+  const handleCityChange = (e) => {
+    setCity(e.target.value);
+  };
+
+  const getWeather = () => {
+    console.log(city);
+    // const url = ``;
+
+    // fetch(url)
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     console.log(data);
+    //   })
+    //   .catch((error) => {
+    //     console.error(error);
+    //   });
+  };
+
+  return (
+    <section>
+      <h1>Weather</h1>
+      <input
+        type="text"
+        id="cityInput"
+        value={city}
+        onChange={handleCityChange}
+      />
+      <button onClick={getWeather}>Get Weather</button>
+      <div id="weatherInfo"></div>
+    </section>
+  );
 }
 
 export default Weather;
