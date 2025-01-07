5ec8ad61e8f292928b8fd716d62ab45122425bd0
Dmitrii <dimterion59@gmail.com>
2025-01-07 12:15:27 +0100

    add Loader component

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,13 @@
+import "./loader.css";
+
+export default function Loader() {
+  return (
+    <section className="loader">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </section>
+  );
+}
