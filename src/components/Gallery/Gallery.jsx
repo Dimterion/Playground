ebf0faa0da913bfa7b9be27158e0d4f0a901c6de
Dmitrii <dimterion59@gmail.com>
2025-01-10 12:12:41 +0100

    adjust gallery component

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,21 +1,21 @@
-import "./gallery.css";
 import cardImg from "../../assets/Card/cardImg.jpg";
+import "./gallery.css";
 
 function Gallery() {
   return (
     <div className="gallery-wrapper">
       <section className="gallery-section">
         <figure className="gallery-figure">
-          <img src={cardImg} alt="Card image" />
+          <img src={cardImg} alt="Card image placeholder" />
         </figure>
         <figure className="gallery-figure">
-          <img src={cardImg} alt="Card image" />
+          <img src={cardImg} alt="Card image placeholder" />
         </figure>
         <figure className="gallery-figure">
-          <img src={cardImg} alt="Card image" />
+          <img src={cardImg} alt="Card image placeholder" />
         </figure>
         <figure className="gallery-figure">
-          <img src={cardImg} alt="Card image" />
+          <img src={cardImg} alt="Card image placeholder" />
         </figure>
       </section>
     </div>
