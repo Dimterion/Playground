56803015d66f7d60540310381610d26277e902e0
Dmitrii <dimterion59@gmail.com>
2025-01-11 10:14:12 +0100

    add new header variant

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,5 +7,45 @@ export default function Header() {
     <Link to="/" aria-label="Back to home page" className="header-link">
       <FaBackward />
     </Link>
+
+    // <header className="header-menu__wrapper">
+    //   <div className="header-menu__bar">
+    //     <Link to="/" className="header-logo">
+    //       PG
+    //     </Link>
+    //     <nav>
+    //       <ul className="header-navigation header-hide">
+    //         <li>
+    //           <button>Menu</button>
+    //           <div className="header-dropdown__wrapper">
+    //             <div className="header-dropdown">
+    //               <ul className="header-ul">
+    //                 <li>
+    //                   <div className="header-item-title">
+    //                     <h3>Item</h3>
+    //                     <p>Item description</p>
+    //                   </div>
+    //                 </li>
+    //               </ul>
+    //             </div>
+    //           </div>
+    //         </li>
+    //       </ul>
+    //     </nav>
+    //   </div>
+    //   <div className="header-action-buttons header-hide">
+    //     <Link to="/" className="header-secondary-btn">
+    //       Home
+    //     </Link>
+    //     <Link to="/" className="header-primary-btn">
+    //       Home
+    //     </Link>
+    //   </div>
+    //   <button
+    //     aria-label="Open menu"
+    //     className="header-menu__btn"
+    //     type="button"
+    //   ></button>
+    // </header>
   );
 }
