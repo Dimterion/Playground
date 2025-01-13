39bbdebdbdf5d90a50bf034a7491662f4195c819
Dmitrii <dimterion59@gmail.com>
2025-01-13 11:55:44 +0100

    adjust header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -69,49 +69,9 @@ export default function Header() {
     //             </div>
     //           </div>
     //         </li>
-    //         <li>
-    //           <button>Menu</button>
-    //           <div className="header-dropdown__wrapper">
-    //             <div className="header-dropdown">
-    //               <ul className="header-ul">
-    //                 <li>
-    //                   <div className="header-item-title">
-    //                     <h3>Item</h3>
-    //                     <p>Item description</p>
-    //                   </div>
-    //                 </li>
-    //                 <li>
-    //                   <div className="header-item-title">
-    //                     <h3>Item</h3>
-    //                     <p>Item description</p>
-    //                   </div>
-    //                 </li>
-    //                 <li>
-    //                   <div className="header-item-title">
-    //                     <h3>Item</h3>
-    //                     <p>Item description</p>
-    //                   </div>
-    //                 </li>
-    //               </ul>
-    //             </div>
-    //           </div>
-    //         </li>
     //       </ul>
     //     </nav>
     //   </div>
-    //   <div className="header-action-buttons header-hide">
-    //     <Link to="/" className="header-secondary-btn">
-    //       Home
-    //     </Link>
-    //     <Link to="/" className="header-primary-btn">
-    //       Home
-    //     </Link>
-    //   </div>
-    //   <button
-    //     aria-label="Open menu"
-    //     className="header-menu__btn"
-    //     type="button"
-    //   >Menu</button>
     // </header>
   );
 }
